b28a3be63397fe183f61938a55ea03e514599549
jokerguo <1977036313@qq.com>
2021-05-14 16:02:07 +0800

    icon 公用组件

---
+++ src/components/Icon.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//引入文件中全部svg
+const requireAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
+try {requireAll(require.context('icons', true, /\.svg$/))}catch(err){console.log(err);}
+
+type Props = {
+  name: string
+}
+
+const Icon = (props: Props) =>{
+  return (
+    <svg className="icon">
+      <use xlinkHref={'#' + props.name}/>
+    </svg>
+  )
+}
+
+export default Icon
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
-import x from '../icons/money.svg';
+import Icon from './Icon';
+
 
 const NavWrapper = styled.nav`
 line-height: 24px;
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
   > ul {
-  display: flex;
+    display: flex;
     > li {
-        width: 33.3333%;
-        text-align: center;
-        padding: 16px;
+      width: 33.3333%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 4px 0;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -21,13 +28,15 @@ function Nav() {
     <NavWrapper>
       <ul>
         <li>
-          <img src={x} alt="嘘嘘"/>
+          <Icon name="tag"/>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <Icon name="money"/>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+         <Icon name="chart"/>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
