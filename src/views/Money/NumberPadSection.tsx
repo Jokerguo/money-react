2540035d937a93997b24239ccd19173d687cd0e1
jokerguo <1977036313@qq.com>
2021-05-15 21:01:47 +0800

    拆分NumberPadSection

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,87 +1,22 @@
-import styled from 'styled-components';
 import React, {useState} from 'react';
-
-const Wrapper = styled.section`
-  > .output{
-    font-size: 32px;
-    background: #fff;
-    line-height: 72px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.5);
-  }
-  > .pad{
-    > button{
-      float: left;
-      height: 64px;
-      width: 25%;
-      border: none;
-      &.ok{float: right;height: 128px;}
-      &.zeo{width: 50%;}
-      &:nth-child(1){
-        background: #f2f2f2;
-      }
-      &:nth-child(2),
-      &:nth-child(5){
-        background: #e0e0e0;
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9){
-        background: #d3d3d3;
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10){
-        background: #c1c1c1;
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13){
-        background: #b8b8b8;
-      }
-      &:nth-child(12){
-        background: #9a9a9a;
-      }
-      &:nth-child(14){
-        background: #a9a9a9;
-      }
-    }
-  }
-`;
+import {Wrapper} from './NumberPadSection/Wrapper';
+import {generateOutput} from './NumberPadSection/generateOutput';
 
 const NumberPadSection:React.FC = ()=>{
-  const [output,setOutput] = useState('0')
+  const [output,_setOutput] = useState('0')
+
+  const setOutput = (output: string)=>{
+    _setOutput(output.length > 16 ? output.slice(0,16) : output)
+  }
 
   const onClickButton = (e: React.MouseEvent)=>{
     const text = (e.target as HTMLButtonElement).textContent
-    switch (text){
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-      case '0':
-      case '.':
-        if(output === '0'){
-          setOutput(text)
-        }else{
-          setOutput(output + text)
-        }
-        break;
-      case '删除':
-        console.log('删除');
-        break;
-      case '清空':
-        console.log('清空');
-        break;
-      case 'OK':
-        console.log('OK');
-        break;
+    if(text === 'OK'){
+      //TODO
+      return
+    }
+    if(text !== null){
+      setOutput(generateOutput(text,output))
     }
   }
 
