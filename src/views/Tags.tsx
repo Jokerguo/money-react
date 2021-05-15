88199a114b434b50a499f510f4f5476fa554f815
jokerguo <1977036313@qq.com>
2021-05-15 10:49:06 +0800

    备注

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -30,7 +30,26 @@ const TagSection = styled.section`
   }
 `;
 const NoteSection = styled.section`
-  
+  background: #f5f5f5;
+  padding: 0 16px;
+  > label{
+    display: flex;
+    align-items: center;
+    > span{
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+    > input{
+      display: block;
+      width: 100%;
+      height: 72px;
+      border: none;
+      background: none;
+      &:focus{
+        outline: none;
+      }
+    }
+  }
 `;
 const CategorySection = styled.section`
   
@@ -54,7 +73,7 @@ function Tags() {
       <NoteSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder="添加备注"/>
         </label>
       </NoteSection>
       <CategorySection>
