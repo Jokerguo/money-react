dc5d230972b93f06ce4cc45c83c2a513aac0c2b3
jokerguo <1977036313@qq.com>
2021-05-16 15:35:40 +0800

    创建createId

--- src/views/Money/TagSection.tsx
+++ src/views/Money/TagSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import React from 'react';
 import {useTags} from 'Hooks/useTags'
+import {createId} from 'lib/createId';
 
 const Wrapper = styled.section`
   flex-grow: 1;
@@ -49,7 +50,7 @@ const TagSection: React.FC<Props> = (props)=>{
   const onAddTag = ()=>{
     const tagName = window.prompt()
     if(!!tagName){
-      setTags([...tags,{id: Math.random(),name: tagName}])
+      setTags([...tags,{id: createId(),name: tagName}])
     }
   }
   const onToggleTag = (tagId: number)=>{
