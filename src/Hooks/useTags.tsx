0b04b2de712a9e244ae612379e97a719b63d4c76
jokerguo <1977036313@qq.com>
2021-05-16 18:28:53 +0800

    编辑标签

--- src/Hooks/useTags.tsx
+++ src/Hooks/useTags.tsx
@@ -11,7 +11,23 @@ const data = [
 const useTags = ()=>{
   const [tags,setTags] = useState<{id: number,name: string}[]>(data)
   const findTag = (id: number)=> tags.filter(tag=> tag.id === id)[0]
-  return {tags,setTags,findTag}
+  const findTagIndex = (id:number)=>{
+    let index = -1
+    for(let i=0; i<tags.length; i++){
+      if(tags[i].id === id){
+        index = i
+        break
+      }
+    }
+    return index
+  }
+  const updateTag = (id: number,value: string)=>{
+    let index = findTagIndex(id)
+    const cloneTags = JSON.parse(JSON.stringify(tags))
+    cloneTags[index].name = value
+    setTags(cloneTags)
+  }
+  return {tags,setTags,findTag,findTagIndex,updateTag}
 }
 
 export {useTags}
--- src/views/Tags/EditTag.tsx
+++ src/views/Tags/EditTag.tsx
@@ -1,17 +1,49 @@
 import React from 'react';
 import {useTags} from 'Hooks/useTags';
 import {useParams} from "react-router-dom";
+import Layout from 'components/Layout';
+import Icon from 'components/Icon';
+import {Button} from 'components/Button';
+import styled from 'styled-components';
+import {Input} from 'components/Input';
 
 type Params = {
   id: string
 }
 
+const TopBar = styled.header`
+  background: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+`
+
+const InputWrapper = styled.div`
+  padding: 0 16px;
+  margin-top: 16px;
+  background: #fff;
+`
+
 const EditTag = ()=>{
-  const {findTag} = useTags()
+  const {findTag,updateTag} = useTags()
   let { id } = useParams<Params>()
   let tag = findTag(parseInt(id))
   return (
-    <div>{tag.name}</div>
+    <Layout>
+      <TopBar>
+        <Icon name="left"/>
+        <span>编辑标签</span>
+        <Icon/>
+      </TopBar>
+      <InputWrapper>
+        <Input label="标签名" type="text" value={tag.name}
+               onChange={(e)=>{updateTag(tag.id,e.target.value)}}
+        />
+      </InputWrapper>
+      <Button name="删除标签"/>
+    </Layout>
   )
 }
 
