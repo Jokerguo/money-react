78d1c8b312c663edbef732d00152168f51837930
jokerguo <1977036313@qq.com>
2021-05-18 21:01:57 +0800

    add gitee仓库

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ cd build &&
 git init &&
 git add . &&
 git commit -m 'deploy' &&
-git remote add origin git@github.com:Jokerguo/money-react-website.git &&
+#git remote add origin git@github.com:Jokerguo/money-react-website.git &&
+git remote add origin git@gitee.com:jokerguo/money-react-website.git &&
 git push -u origin master -f
 cd -
